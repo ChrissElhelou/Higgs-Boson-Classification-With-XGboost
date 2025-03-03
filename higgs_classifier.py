bdfaf278a8660557cfb12bac77195f7e722d9f99
chrieh <chrieh@kth.se>
2025-03-03 14:50:10 +0100

    Uploading code shell/first commit

---
+++ higgs_classifier.py
@@ -0,0 +1,16 @@
+def load_data():
+    pass
+def data_cleaning():
+    pass
+def data_splitting():
+    pass
+def train_model_XGboost():
+    pass
+def model_evaluation():
+    pass
+def shap_generator():
+    pass
+def main():
+    pass
+if __name__ == "__main__":
+    main()
