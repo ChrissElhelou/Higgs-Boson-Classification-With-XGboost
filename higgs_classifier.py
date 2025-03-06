e4d6f83e418f9e3f009e8507a22ab6f6d9ec8adb
chrieh <chrieh@kth.se>
2025-03-06 12:29:22 +0100

    Added data reading and cleaning

--- higgs_classifier.py
+++ higgs_classifier.py
@@ -1,7 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 def load_data():
-    pass
+    """Load the Higgs-Boson dataset from CSV file"""
+    df = pd.read_csv("higgs.csv")
+    return df
 def data_cleaning():
-    pass
+    """Cleans the Higgs-Boson dataset by replacing missing values and converting column objects to numerical values"""
+    df = df.copy()
+    df.replace(-999.0, np.nan, inplace=True)
+    #Remove any rows missing with values and convert object columns
+    df.dropna(inplace=True)
+    for col in df.select_dtypes(include=['object']).columns:
+        df[col] = pd.to_numeric(df[col])
+    print(df.dtypes.value_counts())
+    return df
 def data_splitting():
     pass
 def train_model_XGboost():
@@ -14,3 +28,5 @@ def main():
     pass
 if __name__ == "__main__":
     main()
+
+ 
